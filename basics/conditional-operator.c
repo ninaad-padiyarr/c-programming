759ed33dfd0961d9309fea7a98ba85cf7c181e8e
ninaad-padiyarr <ninaadpadiyarr@gmail.com>
2026-01-08 23:16:43 +0530

    code in ?: operator

--- basics/conditional-operator.c
+++ basics/conditional-operator.c
@@ -1,16 +1,21 @@
 //same code using conditional operator using characters
+
 #include <stdio.h>
 void main()
 {
     char grade;
     int marks;
-    //input hub
+
+    //input hub.
     printf("Enter the marks of the student :");
     scanf("%d", &marks);
+
     //testing hub
     grade = (marks >= 90) ? 'A' :
             (marks >= 75) ? 'B' :
             (marks >= 60) ? 'C' :
             (marks >= 50) ? 'D' : 'F';
+            
+            //output hub.
     printf("Grade %c\n", grade);
 }
